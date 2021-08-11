86e1b46a75859db8eca369a6d8011d91f2638191
jameshsu1125 <70932507+jameshsu1125@users.noreply.github.com>
2021-08-11 22:45:02 +0800

    add glow effect

--- src/Background/animation.js
+++ src/Background/animation.js
@@ -2,25 +2,30 @@ import Tweener, { Bezier } from 'lesca-object-tweener';
 
 export default class Animation {
 	constructor(props) {
-		const { container, glow } = props;
-		const duration = 24000 * 3;
+		const { container, glow, glow2, glow3 } = props;
+		const duration = 24000 * 2;
 
 		this.tr = {
 			init() {
 				this.container.init();
 				this.glow.init();
+				this.glow2.init();
+				this.glow3.init();
 			},
 			in() {
 				this.container.in();
 				this.glow.in();
+				this.glow2.in();
+				this.glow3.in();
 			},
-			glow: {
+			glow3: {
 				delay: 0,
-				property: { opacity: 1, rotate: 0 },
+				property: { opacity: 1, rotate: -20 },
 				unit: { opacity: '', rotate: 'deg' },
 				easing: Bezier.linear,
+				blank: 0.3,
 				init() {
-					this.c = glow.current;
+					this.c = glow3.current;
 					this.tweener = new Tweener();
 					this.tran();
 				},
@@ -28,7 +33,48 @@ export default class Animation {
 					const { property, delay, easing } = this;
 					const { opacity, rotate } = property;
 					const from = { opacity, rotate };
-					const to = { opacity: 1, rotate: 180 };
+					const to = { opacity: 1, rotate: 30 };
+					this.tweener
+						.add({
+							from,
+							to,
+							delay,
+							easing,
+							duration: duration / 5,
+							onUpdate: (e) => this.tran(e),
+							onComplete: (e) => this.tran(e),
+						})
+						.play();
+				},
+				tran(e = this.property) {
+					this.property = { ...this.property, ...e };
+					const cssText = Object.entries(this.property).map((css) => {
+						const [key, value] = css;
+						const unit = this.unit[key] || '';
+						if (key === 'rotate') return `transform:rotate(${value}${unit});`;
+						if (key === 'opacity') return `opacity:${value - this.blank - Math.random() * 0.2};`;
+						return `${key}:${value}${unit};`;
+					});
+
+					this.c.style.cssText = cssText.join('');
+				},
+			},
+			glow2: {
+				delay: 0,
+				property: { opacity: 1, rotate: 0 },
+				unit: { opacity: '', rotate: 'deg' },
+				easing: Bezier.linear,
+				blank: 0.2,
+				init() {
+					this.c = glow2.current;
+					this.tweener = new Tweener();
+					this.tran();
+				},
+				in() {
+					const { property, delay, easing } = this;
+					const { opacity, rotate } = property;
+					const from = { opacity, rotate };
+					const to = { opacity: 1, rotate: 30 };
 					this.tweener
 						.add({
 							from,
@@ -47,7 +93,48 @@ export default class Animation {
 						const [key, value] = css;
 						const unit = this.unit[key] || '';
 						if (key === 'rotate') return `transform:rotate(${value}${unit});`;
-						if (key === 'opacity') return `opacity:${value - Math.random() * 0.1};`;
+						if (key === 'opacity') return `opacity:${value - this.blank - Math.random() * 0.1};`;
+						return `${key}:${value}${unit};`;
+					});
+
+					this.c.style.cssText = cssText.join('');
+				},
+			},
+			glow: {
+				delay: 0,
+				property: { opacity: 1, rotate: -30 },
+				unit: { opacity: '', rotate: 'deg' },
+				easing: Bezier.linear,
+				blank: 0,
+				init() {
+					this.c = glow.current;
+					this.tweener = new Tweener();
+					this.tran();
+				},
+				in() {
+					const { property, delay, easing } = this;
+					const { opacity, rotate } = property;
+					const from = { opacity, rotate };
+					const to = { opacity: 1, rotate: 60 };
+					this.tweener
+						.add({
+							from,
+							to,
+							delay,
+							easing,
+							duration: duration / 2,
+							onUpdate: (e) => this.tran(e),
+							onComplete: (e) => this.tran(e),
+						})
+						.play();
+				},
+				tran(e = this.property) {
+					this.property = { ...this.property, ...e };
+					const cssText = Object.entries(this.property).map((css) => {
+						const [key, value] = css;
+						const unit = this.unit[key] || '';
+						if (key === 'rotate') return `transform:rotate(${value}${unit});`;
+						if (key === 'opacity') return `opacity:${value - this.blank - Math.random() * 0.1};`;
 						return `${key}:${value}${unit};`;
 					});
 
--- src/Background/main.js
+++ src/Background/main.js
@@ -8,16 +8,20 @@ const Background = (props) => {
 
 	const container = useRef();
 	const glow = useRef();
+	const glow2 = useRef();
+	const glow3 = useRef();
 	const animation = useRef();
 
 	useEffect(() => {
-		animation.current = new Animation({ container, glow });
+		animation.current = new Animation({ container, glow, glow2, glow3 });
 		animation.current.in();
 	}, []);
 
 	return (
 		<div ref={container} className='Background'>
 			<div ref={glow} className='glow' />
+			<div ref={glow2} className='glow2' />
+			<div ref={glow3} className='glow3' />
 			{commingSoon && <div className='darken' />}
 		</div>
 	);
