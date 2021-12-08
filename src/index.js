b5b52d4179fd51d6cb9acca10a3fa16ebfcef304
jameshsu1125 <70932507+jameshsu1125@users.noreply.github.com>
2021-12-08 13:49:19 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import './Setting/global.less';
 Facebook.install(process.env.FB_ID);
 Gtag.install(process.env.GA_ID);
 Click.init(true, true);
+console.log('[update] Wed Dec 08 2021 13:44:03 GMT+0800 (台北標準時間)');
 
 render(
 	<>
