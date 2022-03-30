18b32e1244ffb2d25298cf7ad4905c2b4b35c6cc
jameshsu1125 <jameshsu1125@gmail.com>
2022-03-30 13:08:05 +0800

    remove "朗讀" of select page

--- src/Select/main.js
+++ src/Select/main.js
@@ -18,7 +18,6 @@ const device = window.innerWidth <= 750;
 const storyTarget = QueryString.get('t');
 const sliderSteps = QueryString.get('s');
 const hashFromFacebook = window.location.hash === '#_=_';
-const queryIsVR = QueryString.get('vr');
 
 const settings = {
 	dots: true,
@@ -170,7 +169,6 @@ const Select = forwardRef((props, ref) => {
 						為了更沉浸地感受
 						<br />
 						請輕觸螢幕播放故事
-						{queryIsVR ? '' : '朗讀'}
 					</div>
 				</Instruct>
 			)}
